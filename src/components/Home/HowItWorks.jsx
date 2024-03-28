7281a4c750e260c9ac80e07637cc61bf34e53ffd
Akshay kumar singh <akshaysing975@gmail.com>
2024-03-28 21:40:00 +0530

    first commit

---
+++ src/components/Home/HowItWorks.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { FaUserPlus } from "react-icons/fa";
+import { MdFindInPage } from "react-icons/md";
+import { IoMdSend } from "react-icons/io";
+
+const HowItWorks = () => {
+  return (
+    <>
+      <div className="howitworks">
+        <div className="container">
+          <h3>How JobZee Works</h3>
+          <div className="banner">
+            <div className="card">
+              <FaUserPlus />
+              <p>Create Account</p>
+              <p>
+                At first create your account with your mail or phone number on Tech Jobs portal.
+              </p>
+            </div>
+            <div className="card">
+              <MdFindInPage />
+              <p>Find a Job/Post a Job</p>
+              <p>
+                If you are an employer than post jobs otherwise find jobs according to your interest and skills.
+              
+              </p>
+            </div>
+            <div className="card">
+              <IoMdSend />
+              <p>Apply For Job/Recruit Suitable Candidates</p>
+              <p>
+              Apply for jobs in your interested fields and start your carrier.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HowItWorks;
